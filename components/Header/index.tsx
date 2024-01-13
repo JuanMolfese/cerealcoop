736b7344504c6cf6db4f3d1e576f78195897d662
JuanMolfese <juanjosemolfese@gmail.com>
2024-01-13 16:23:50 -0300

    se dejo por defecto modo white, en el archivo : providers.tsx del raiz. Y se ejecuta  setNavbarOpen(false) en el onclick de cada li del menu mobile.

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -106,7 +106,7 @@ const Header = () => {
                 >
                   <ul className="block lg:flex lg:space-x-12">
                     {menuData.map((menuItem, index) => (
-                      <li key={index} className="group relative">
+                      <li key={index} className="group relative" onClick={() => setNavbarOpen(false)}>
                         {menuItem.path ? (
                           <Link
                             href={menuItem.path}
