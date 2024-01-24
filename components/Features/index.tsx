fd385d196d6301283592b3369e01eedd5a5097d1
JuanMolfese <juanjosemolfese@gmail.com>
2024-01-24 15:13:20 -0300

    se agrega acento en instantanea

--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -93,7 +93,7 @@ const Features = () => {
                   />
               </div>
               <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
-                Avena Instantanea
+                Avena Instantánea
               </h3>
               <p className="pr-[10px] text-base font-medium leading-relaxed text-body-color">
                 Contiene 350 gramos. Caja por 22 unidades.
